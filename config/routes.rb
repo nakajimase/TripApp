77d5c88187f2ee860a4818b406e781d01dccb8c0
root <root@ip-172-31-34-24.us-east-2.compute.internal>
2018-07-03 14:11:52 +0000

    Modify showing web_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#match 'article' => to: 'article#hello', via: [:get, :post]
+#match 'article' => 'article#hello'
+#get   '/article', to: 'article#hello'
+#match 'article/hello' => 'article#hello' :via => :get
+match 'article/hello', to: 'article#hello', via: :get
+
 end
