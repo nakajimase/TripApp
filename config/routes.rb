13dddc4bca278ecd4c185dc6deebfcd8d6264c51
nakajimase <seiya.nakajima.jpn@gmail.com>
2018-08-28 21:40:06 +0900

    メールアドレスによるユーザ新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ match 'article/hello', to: 'article#hello', via: :get
 match 'article/getList', to: 'article#getList', via: :get
 match 'article/getDetail/:article_id', to: 'article#getDetail', via: :get
 
+match 'user/hello', to: 'user#hello', via: :get
+#match 'user/add/:email/:password', to: 'user#userAdd', via: :get
+#match 'user/add', to: 'user#userAdd', via: :get
+match 'user/add', to: 'user#userAdd', via: :post
+
 end
